b9bc7eac444fcc9825186ee5a2169fb714ee5383
Laura Paskowski <70514748+lap8066@users.noreply.github.com>
2023-07-20 12:32:08 +0000

    first try

--- 3_Java_Minimalist/Challenge.java
+++ 3_Java_Minimalist/Challenge.java
@@ -1,6 +1,13 @@
 public class Challenge {
 
     String weightedSort(String text) {
+        String[] currencies = text.split(" ");
+        List new = new ArrayList;
+        for (int i = 0; i < currencies.size(); i++) {
+            String element = currencies.get(i);
+            str = element.replaceALL("[^a-zA-Z0-9_-]");
+
+        }
         return "";
     }
 
